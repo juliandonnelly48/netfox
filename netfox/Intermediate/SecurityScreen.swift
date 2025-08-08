7fe6e5f89d98295330711eb1bac00ec3ebc8e05c
user1000 <user1000@unlabeled.qupra.nl>
2025-08-08 17:25:03 +0200

    logic change 6

--- netfox/Intermediate/SecurityScreen.swift
+++ netfox/Intermediate/SecurityScreen.swift
@@ -401,7 +401,7 @@ private extension InterScreen {
                 .resizable()
                 .frame(width: 20, height: 20)
                 .foregroundColor(.gray)
-                .offset(x: isIpad ? 250 : 150)
+                .offset(x: isIpad ? 200 : 130)
             
             VStack(spacing: 8) {
                 KFImage(URL(string: scanObject.messIcon))
